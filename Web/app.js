a941f9039a667c794b97b79e0012526477e7a59f
majkelsch <m.schenk2006@seznam.cz>
2022-11-30 20:00:22 +0100

    30.11.
    Idk něco jsem opravil a optimalizoval

--- Web/app.js
+++ Web/app.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openMenu() {
     document.getElementById("myMenu").style.width = "100%";
-  }
+}
 
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeMenu() {
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeMenu() {
     document.getElementById("myMenu").style.width = "0%";
-  }
+}
